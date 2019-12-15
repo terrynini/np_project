a389665afba44c5cb3122c24cd78c22f41e5490c
terrynini <terrynini38514@gmail.com>
2019-12-16 03:18:17 +0800

    OKK

--- boost asio example/console.cpp
+++ boost asio example/console.cpp
@@ -1,7 +1,8 @@
 #include <array>
-#include <boost/asio.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/asio.hpp>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <memory>
 #include <unistd.h>
@@ -10,18 +11,33 @@
 using namespace std;
 using namespace boost::asio;
 
-class shellSession : public enable_shared_from_this<shellSession>
-{
+class shellSession : public enable_shared_from_this<shellSession> {
 private:
-    enum
-    {
-        max_length = 1024
-    };
+    enum { max_length = 1024 };
+    ip::tcp::endpoint _ep;
     ip::tcp::socket _socket;
     array<char, max_length> _data;
 
 public:
-    shellSession(ip::tcp::socket socket) : _socket(move(socket)) {}
+    std::string server;
+    std::string inputfile;
+    std::string tag;
+    std::string port;
+    std::fstream commands;
+    shellSession(ip::tcp::endpoint ep, boost::asio::io_service& io_service)
+        : _ep(move(ep))
+        , _socket(io_service)
+    {
+    }
+    void connect()
+    {
+        auto self(shared_from_this());
+        commands.open("./test_case/" +inputfile, std::fstream::in);
+        _socket.async_connect(_ep, [this, self](const boost::system::error_code& error) {
+            if (!error)
+                this->do_read();
+        });
+    }
 
 private:
     void do_read()
@@ -30,27 +46,135 @@ private:
         _socket.async_read_some(
             buffer(_data, max_length),
             [this, self](boost::system::error_code ec, size_t length) {
-                if (!ec)
-                    do_write(length);
+                if (!ec) {
+                    std::string message = std::string(_data.data(), length);
+                    boost::replace_all(message, "\n", "&NewLine;");
+                    boost::replace_all(message, "\"", "\\\"");
+                    boost::replace_all(message, "\'", "\\\'");
+                    cout << "<script>document.getElementById('" << tag << "').innerHTML += '" << message << "';</script>" << endl;
+                    if (message.find("% ") != string::npos) {
+                        do_read();
+                    } else {
+                        do_write();
+                    }
+                }
             });
     }
 
-    void do_write(size_t length)
+    void do_write()
     {
+        std::string cmd;
+        commands >> cmd;
+        cmd += "\n";
         auto self(shared_from_this());
         _socket.async_send(
-            buffer(_data, length),
-            [this, self](boost::system::error_code ec, size_t /* length */) {
-                if (!ec)
+            buffer(cmd, cmd.length()),
+            [this, self, cmd](boost::system::error_code ec, size_t /* length */) {
+                if (!ec){
+                    cout << "<script>document.getElementById('" << tag << "').innerHTML += '<b>" << cmd << "</b>';</script>" << endl;
                     do_read();
+                }
             });
     }
 };
 
-int main(int argc, char** argv, char** envp){
-    cout << "Content-type: text/html" << endl << endl;
-    for(int i = 0 ; envp[i] ; i++){
-        cout << envp[i] <<  endl;        
+extern char** environ;
+
+void htmlbody();
+
+std::vector<shared_ptr<shellSession>> slist;
+std::map<std::string, std::string> GET_;
+
+int main(int argc, char** argv, char** envp)
+{
+    io_service io_service;
+    cout << "Content-type: text/html" << endl
+         << endl;
+    ip::tcp::resolver resolver(io_service);
+    ip::tcp::resolver::iterator end;
+    std::string query(getenv("QUERY_STRING"));
+    std::vector<std::string> parameter;
+    boost::split(parameter, query, [](char c) { return c == '&'; });
+    for (auto i : parameter) {
+        std::vector<std::string> temp;
+        boost::split(temp, i, [](char c) { return c == '='; });
+        GET_[temp[0]] = temp[1];
     }
-    cout << "endl";
+    for (int i = 0; i < 4; i++) {
+        if (GET_["h" + to_string(i)] != "" && GET_["p" + to_string(i)] != "" && GET_["f" + to_string(i)] != "") {
+            ip::tcp::resolver::iterator ei = resolver.resolve(ip::tcp::resolver::query(GET_["h" + to_string(i)], GET_["p" + to_string(i)]));
+            if (ei != end) {
+                shared_ptr<shellSession> add = make_shared<shellSession>(move(*ei), io_service);
+                slist.push_back(add);
+                add->server = GET_["h" + to_string(i)];
+                add->inputfile = GET_["f" + to_string(i)];
+                add->tag = "s" + to_string(slist.size() - 1);
+                add->port = GET_["p" + to_string(i)];
+                add->connect();
+            }
+        }
+    }
+    htmlbody();
+    io_service.run();
+}
+
+void htmlbody()
+{
+    cout << R"(
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>NP Project 3 Console</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Code+Pro"
+      rel="stylesheet"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      href="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678068-terminal-512.png"
+    />
+    <style>
+      * {
+        font-family: 'Source Code Pro', monospace;
+        font-size: 1rem !important;
+      }
+      body {
+        background-color: #212529;
+      }
+      pre {
+        color: #cccccc;
+      }
+      b {
+        color: #ffffff;
+      }
+    </style>
+  </head>
+  <body>
+    <table class="table table-dark table-bordered">
+      <thead>
+        <tr>)";
+    for (auto s : slist)
+        cout << R"(<th scope = "col"> )" << s->server << " : " << s->port << " </th >" << endl;
+    cout << R"(
+        </tr>
+      </thead>
+      <tbody>
+        <tr>)";
+    for (auto s : slist)
+        cout << R"(<td><pre id = ")" << s->tag << R"(" class = "mb-0"></pre></td>)" << endl;
+    cout << R"(
+        </tr>
+      </tbody>
+    </table>
+     </body>
+    </html>
+)";
 }
--- boost asio example/http_server.cpp
+++ boost asio example/http_server.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <memory>
+#include <map>
 #include <unistd.h>
 #include <utility>
 
@@ -11,7 +12,6 @@ using namespace std;
 using namespace boost::asio;
 
 io_service global_io_service;
-char **envp;
 
 class HttpSession : public enable_shared_from_this<HttpSession>
 {
@@ -22,6 +22,7 @@ private:
     };
     ip::tcp::socket _socket;
     array<char, max_length> _data;
+    map<string, string> _env;
 
 public:
     HttpSession(ip::tcp::socket socket) : _socket(move(socket)) {}
@@ -42,12 +43,12 @@ private:
     void parseHttp()
     {
         std::vector<std::string> lines;
-        std::vector<std::string> now;
+
         iter_split(lines, _data, boost::algorithm::first_finder("\r\n"));
-        split(now, lines[0], boost::is_space());
-        if (now[0] != "GET")
+        this->ParseHttpLines(lines);
+        if (_env["REQUEST_METHOD"] != "GET")
         {
-            std::string message("HTTP/1.1 404\r\n\r\n");
+            std::string message("HTTP/1.1 418\r\n\r\nNO I'm a teapot\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN000KKKKKXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx;,:ccccoocl0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd...;:\'..,oc.;0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK;...;c\'...\'\'..lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK;.............cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO;...........c0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMWNXKOOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNNNNXXXXXKd. ... ....:0XXXNNNWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMWXOxxxoc;;colxXWMMMMMMMMMMMMMMMMMMMMMMMMMMWNXKKKKKKK000000000Od;..........\'oO0000KK00KXXXXK00XWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMW0o;,,;\'...:c;cOWMMMMMMMMMMMMMMMMMMMMMMMNX0xxkkkkkkxxxxxxxxxdd:...\'.....\'..,oxxkkkkOOOO0000OdloOXWMMMMMMMMMMMMMMMMMMMMMWNXXXXXNNWMMMMMMMMMMMMMMM\nMMMMMMMMW0d:;,....\',.,dNMMMMMMMMMMMMMMMMMMMMMNkoxl:;;;:clooddddxxxdddoooolcccldolodddddddddooooooddc:oO0NMMMMMMMMMMMMMMMMMNKkxdolllodxO0XWMMMMMMMMMMMM\nMMMMMMMMMMNkc,\'........oNMMMMMMMMMMMMMMMMMMMMWk;\'\'\'\'\',,;:ccllooddxkkkkkOOOOOOOOOOOOOOOOOkxolc:;,,;;;:cldKMMMMMMMMMMMMMMWXkc:::;,\'\'\'\'\',:cldONMMMMMMMMMM\nMMMMMMMMMMMWOc\'........\'xWMMMMMMMMMMMMMMMMWNXKkc\'.....\',,,\'\'\',,;clooollccccccccccclodxxxdocc:;,,,,\'...,dKWMMMMMMMMMMMMNk:\'\'\',:okO0KOxl,.,;:dKWMMMMMMMM\nMMMMMMMMMMMMWk;.........,OWMMMMMMMMMMMMWNK0Oxd:................\',;;;;;,,\'\'\'\'\'\'\'\'\',,\',;;;;;;,\'.........,okKXNWMMMMMMMW0c\'...:xXWMMMMMMWKd,.\'\':OWMMMMMMM\nMMMMMMMMMMMMMNo\'.........cXMMMMMMMMMWNKOkxdool:\'.\'\'\'.................................................\':oxkO0KXNWMMWXd,\'\'.\'lKWMMMMMMMMMMWKc...,kWMMMMMM\nMMMMMMMMMMMMMMk,..........xWMMMMMMWX0kdolcc:;,;;coddooc;,\'\'...\',;::;;,\'.............\';ccclcc:,;:loddl;;:loxkkOO0KKkc,,...lXMMMMMMMMMMMMMMXc...;0MMMMMM\nMMMMMMMMMMMMMM0;..........:KMMMMMN0xoc;,\'\'...,coooddddddoollcccclllcc::;,,,,,,,,,,;;:oddxxddddxkkOkkkxoc;;:lodxxddl;,...;0MMMMMMMMMMMMMMMM0,...oNMMMMM\nMMMMMMMMMMMMMMK:...........dNMMWXxl:,........\':lllooooddddxxxkkkkkkxxxxxxxxxxdxxxxxxxkkOOOOOOkkkkkkkkkkxdl:;;:c;,;::\'...lXMMMMMMMMMMMMMMMMNl...;0MMMMM\nMMMMMMMMMMMMMMXc........;,.,xXXOc,.............;:cclllloooodddddxxxxxxkkkkkkkkkkkkkkkkkkkkkkkxxxxxxxxxxdoc;\'.\'\'..\',,....oNMMMMMMMMMMMMMMMMMk...\'OMMMMM\nMMMMMMMMMMMMMMNl....\'\'..\':,,odl,.................,:::cccclllloooooddddddxxxxxxxxxxxxxxxxdxddddddddddol:,\'.......\',;:;,..dWMMMMMMMMMMMMMMMMMO\'..\'kMMMMM\nMMMMMMMMMMMMMMWo.....:;...,cdc,....................\',,,,,\'\'\'\'\'\',,,,,,,,,,;;;;;;;:::ccccllloooooool:;,.............\';:lll0MMMMMMMMMMMMMMMMMMO\'..,kMMMMM\nMMMMMMMMMMMMMMWd.....\'od:..\',............................................................\'\',;::;\'...................,:ld0WMMMMMMMMMMMMMMMMMO\'..,OMMMMM\nMMMMMMMMMMMMMMMk......;xko,..........................................................................................,:lkXMMMMMMMMMMMMMMMMWx...:KMMMMM\nMMMMMMMMMMMMMMMO,......,oko,..........................................................................................,cdKWMMMMMMMMMMMMMMMNl..\'lNMMMMM\nMMMMMMMMMMMMMMMK:........:c;..........................................................................................\':oOWMMMMMMMMMMMMMMMO,..,kMMMMMM\nMMMMMMMMMMMMMMMNo......................................................................................................;lkNMMMMMMMMMMMMMMXl..\'cKMMMMMM\nMMMMMMMMMMMMMMMWO;.....................................................................................................\'cxXMMMMMMMMMMMMMNd...;kWMMMMMM\nMMMMMMMMMMMMMMMMXo,\'\'\'\'\'................................................................................................:xXMMMMMMMMMMMMNo...,xNMMMMMMM\nMMMMMMMMMMMMMMMMMKo;;,,,,,,,............................................................................................:xXMMMMMMMMMMWKl...,dNMMMMMMMM\nMMMMMMMMMMMMMMMMMWKdc:;;;;;;.\',\'........................................................................................;kNMMMMMMMMMXx;...;xNMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMNkl::;;;;,,;,\'.......................................................................................,kWMMMMMMWXk:...\'cOWMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMWXOdc::;;:c;,\'\'.....................................................................................,OMMMWWX0d;....;dXMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMNKOxoodo:;,,\'\'\'\'..............\'\'\'\'\'..............................................................,kK0Oxo:\'....;o0WMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMWNXKkl:;;,,\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\',,,,,,,,\'\'\'...............\'\'\'\'..\'\'...\'\'.....................,\'..::,\'.....\':dKWMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXxl:;;,,,,,,,\'\'\'\'\'\'\'\'\'\',,,,,,;;;;;,,,,,\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'...............,;\'......\',,:oOXWMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXxlc:;;;,,,,,,,,,,,,,,,,;;;;;;;;;;;;,,,,\'\',,,,,\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'.............,:c,....\';cdOXWMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXklc:;;;;;;;;;,,,,,,,,;;;;;;;;;;;;;;;,,,,,,,,,,,,,,,,,,\',,,,,,,,,,\'\'\'\'...........\',cddllodk0XWMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNOoc:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\'\'\'.........\',;lxKNNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKxl::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,\'\'\'\'\'\'\'\'\'\',,;:o0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0dc::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,\'\'\'\',,,,;;:oOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0xl::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;;;;:coOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXkoc:;;;;,,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,,;;;;;,,,,;;;;;;:::clx0NWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOdc::;;;,,,,,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::coxOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0koc:;:::;;;;;;;;;;;;;;;;;::::::::::::::::::::::::::c:cldk0XNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0kdlc:;;;;;;;;;;;;;;;;;;::::::::::::::::::::;;;;:ldxOKNNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNXK0kxdolc::;;,,,\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\',,,;;:clodk0KNNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNNNXXKK0OkkxxddoooooooooodddxxkkO00KXNWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
             do_write(message, message.length());
             _socket.close();
         }
@@ -55,17 +56,14 @@ private:
         {
             std::string message("HTTP/1.1 200 OK\r\n");
             do_write(message, message.length());
-            int pos = now[1].find("?");
-            string uri = now[1][0] == '/' ? now[1].substr(1) : now[1];
-            if (pos > 0)
-                uri = uri.substr(0, pos - 1);
-            string query = pos > 0 ? now[1].substr(pos) : "";
-            do_fork(uri, query);
+            do_fork(_env["REQUEST_URI"], _env["QUERY_STRING"]);
         }
     }
 
     void do_fork(string cgi, string query)
     {
+        cgi = cgi.substr(1);
+        //truncat "/"
         if (cgi == "")
             cgi = "panel.cgi";
 
@@ -85,7 +83,6 @@ private:
             dup2(fd, 2);
             close(fd);
             char *argv[] = {const_cast<char *>(cgi.c_str()), NULL};
-            setenv("QUERY_STRING", query.c_str(), 1);
             execv(cgi.c_str(), argv);
             // ---
             cout << " FAIL ";
@@ -101,6 +98,38 @@ private:
                 if (!ec)
                     do_read();
             });
+    }
+    void ParseHttpLines(std::vector<std::string> request)
+    {
+        std::vector<std::string> now;
+        split(now, request[0], boost::is_space());
+        setenv("REQUEST_METHOD", now[0]);
+        setenv("SERVER_PROTOCOL", "HTTP/1.1");//now[2]);
+        setenv("REMOTE_ADDR", _socket.remote_endpoint().address().to_string());
+        setenv("REMOTE_PORT", to_string(_socket.remote_endpoint().port()));
+        setenv("SERVER_ADDR", _socket.local_endpoint().address().to_string());
+        setenv("SERVER_PORT", to_string(_socket.local_endpoint().port()));
+        std::vector<std::string> temp;
+        boost::split(temp, now[1], [](char c) { return c == '?'; });
+        setenv("REQUEST_URI", temp[0]);
+        setenv("QUERY_STRING", temp.size() > 1 ? temp[1] : "");
+        request.erase(request.begin());
+        for (auto line : request)
+        {
+            auto idx = line.find_first_of(":");
+            if( idx != std::string::npos){
+                setenv( "HTTP_"+line.substr(0,idx),line.substr(idx+1));
+            }
+        }
+    }
+
+    void setenv(std::string name, std::string value)
+    {
+        boost::to_upper(name);
+        boost::replace_all(name,"-","_");
+        boost::algorithm::trim(value);
+        ::setenv(name.data(), value.data(), 1);
+        _env[name] = value;
     }
 };
 
@@ -129,9 +158,9 @@ private:
     }
 };
 
-int main(int argc, char *const argv[], char **envp)
+int main(int argc, char *const argv[])
 {
-    ::envp = envp;
+    clearenv();
     if (argc != 2)
     {
         cerr << "Usage:" << argv[0] << " [port]" << endl;
